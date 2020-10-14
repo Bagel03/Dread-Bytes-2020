5f718dbf9ff3a4e91013e81d5bcdc1996d15d507
Node-Kid <72894582+Node-Kid@users.noreply.github.com>
2020-10-14 18:41:11 -0400

    Update placeholder.java

--- placeholder.java
+++ placeholder.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int f = 5;
+        int t = 10;
+        int you = f + t;
+        System.out.println(you);
     }
 }
